4a7f918df49539e67c1e42dbed09de01789d26c9
Gahtee <gabrieltimoteopessoal@gmail.com>
2026-06-03 01:29:32 -0300

    so vai

--- badapple.py
+++ badapple.py
@@ -1,1 +1,44 @@
-print("Olá, mundo")
+import cv2
+import os
+import time
+
+video = cv2.VideoCapture("badapple.mp4")
+
+fps = video.get(cv2.CAP_PROP_FPS)
+delay = 1 / fps
+LARGURA = 63
+input()
+while True:
+    ret, frame = video.read()
+
+    if not ret:
+        break
+
+    altura_original, largura_original = frame.shape[:2]
+
+    altura = int((altura_original / largura_original) * LARGURA * 0.5)
+
+    frame = cv2.resize(frame, (LARGURA, altura))
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    texto = []
+
+    for y in range(altura):
+        linha = ""
+
+        for x in range(LARGURA):
+            if gray[y, x] > 127:
+                linha += "⬜"
+            else:
+                linha += "⬛"
+
+        texto.append(linha)
+
+    os.system("cls")  # Windows
+
+    print("\n".join(texto))
+
+    time.sleep(delay-0.009999999999999)
+
+video.release()
